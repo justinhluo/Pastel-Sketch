ca8c9794cf3b25757781913717907d639461b47d
justinhluo <33584797+justinhluo@users.noreply.github.com>
2025-06-12 23:39:24 -0700

    added color selector and rainbow

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let grid = true;
 let size = 15;
+let color = document.querySelector(".colorPicker").value;
+let isRainbow = false;
+// let rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+let rainbowColors = ["rgb(200, 190, 255)","rgb(222, 168, 248)", "rgb(168, 222, 258)","rgb(189, 250, 205)",
+         "rgb(243, 250, 189)","rgb(250, 227, 189)","rgb(248, 172, 171)","rgb(254, 170, 212)"]
+let rainbowIndex = 0;
 
 createBoard(size);
 
@@ -18,6 +24,14 @@ function decreasePixels() {
     createBoard(size);
     }
 }
+ let isDrawing = false;
+
+document.addEventListener("mousedown", () => isDrawing = true);
+    
+
+document.addEventListener("mouseup", () => isDrawing = false);
+    
+
 
 function createBoard(size) {
     const body = document.querySelector("body");
@@ -28,10 +42,29 @@ function createBoard(size) {
         row.classList.add("row");
         for(let j = 0; j < size; j++) {
             const tile = document.createElement("div");
+            tile.setAttribute("draggable", "false");
+            tile.style.userSelect = "none";
             tile.classList.add("tile");
             if(!grid) tile.style.border = 0;
-            tile.addEventListener("mouseover", function(e){
-                e.target.style.background = "grey";
+            tile.addEventListener("mousedown", e => {
+                if(isRainbow) {
+                    e.target.style.background = rainbowColors[rainbowIndex];
+                    if(++rainbowIndex == 8) rainbowIndex = 0;
+
+                }else{
+                    e.target.style.background = color;
+                }
+                
+                
+            });
+            tile.addEventListener("mouseover", e => {
+                 if(isRainbow && isDrawing) {
+                    e.target.style.background = rainbowColors[rainbowIndex];
+                    if(++rainbowIndex == 8) rainbowIndex = 0;
+
+                }else{
+                    if(isDrawing) e.target.style.background = color;
+                } 
             });
             row.appendChild(tile);
         }
@@ -62,5 +95,24 @@ document.querySelector(".increase").addEventListener("click", increasePixels);
 document.querySelector(".decrease").addEventListener("click", decreasePixels);
 
 
+const colorPicker = document.querySelector(".colorPicker");
 
+colorPicker.addEventListener("input", (e) => {
+    color = e.target.value;
 
+});
+
+const rainbow = document.querySelector(".rainbow");
+
+rainbow.addEventListener("click", () => isRainbow = !isRainbow);
+
+rainbow.addEventListener("click", function (e) {
+    if(isRainbow){
+        e.target.style.background = `linear-gradient(to right,rgb(200, 190, 255) 0%,rgb(222, 168, 248) 10%,rgb(168, 222, 258) 30%,rgb(189, 250, 205) 42%,
+         rgb(243, 250, 189) 58%,rgb(250, 227, 189) 70%,rgb(248, 172, 171) 95%,rgb(254, 170, 212) 100%)`;
+        
+    }else {
+        e.target.style.background = "white";
+    }    
+
+});
